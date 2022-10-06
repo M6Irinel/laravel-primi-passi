98da23c8a2c299f7fa9f14799955cbd80c335d61
M6Irinel <momoloacairinel666@gmail.com>
2022-10-06 14:20:02 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel primi passi</title>
+
+    <!-- New version -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/M6Irinel/Framkcss@7afed426c3eb96b21f164839fc10279163084c3c/Framkcss.css">
+
+</head>
+<body class="reset">
+    <header class="border-b-gray-1 border-b-2">
+        <section>
+            <div class="container">
+                <h1 class="p-3 fs-7">Hello world</h1>
+            </div>
+        </section>
+    </header>
+</body>
+</html>
